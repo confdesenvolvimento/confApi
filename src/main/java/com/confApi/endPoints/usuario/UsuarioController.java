e4189f7b3d87721029708614ab06151d44050c00
CONFIANCA\helder.figueiredo <heldertaques@hotmail.com>
2026-06-03 10:22:14 -0400

    Ajuste de login

--- src/main/java/com/confApi/endPoints/usuario/UsuarioController.java
+++ src/main/java/com/confApi/endPoints/usuario/UsuarioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/clube/usuario")
+@RequestMapping("/api/usuario")
 public class UsuarioController {
 
     @Autowired
